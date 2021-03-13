103e6c4f4da98148f925ad124075c14abef24978
Adil Rahman <adildsw@gmail.com>
2021-03-13 00:00:59 -0500

    Initial Commit

---
+++ stepup/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  stepup
+//
+//  Created by Adil Rahman on 3/13/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ stepup/stepupApp.swift
@@ -0,0 +1,17 @@
+//
+//  stepupApp.swift
+//  stepup
+//
+//  Created by Adil Rahman on 3/13/21.
+//
+
+import SwiftUI
+
+@main
+struct stepupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
